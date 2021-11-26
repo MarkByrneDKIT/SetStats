35488fddbddaebf3f010cd987c3515b0a7c809ec
Mark Byrne <73395028+MarkByrneDKIT@users.noreply.github.com>
2021-11-26 12:32:19 +0000

    Update xy queries
    added query to grab linestring in json format for website

--- SETSTATS_SQL_queries.sql
+++ SETSTATS_SQL_queries.sql
@@ -43,6 +43,7 @@ SELECT * FROM history WHERE trainee_id = 1
 
 --Select the lift bar co-ordinates in current lift by id of lift
 SELECT ST_AsText(xy) FROM `current_session` JOIN `current_lift` WHERE lift_id = 1    -- NOW DISPLAYING CORRECLTY NOT .BIN FILE
+SELECT ST_AsGeoJSON(xy) FROM `current_session` JOIN `current_lift` WHERE lift_id = 1
 
 
 --https://stackoverflow.com/questions/34911046/getting-geojson-linestring-from-mysql-geometry-wkt-data
