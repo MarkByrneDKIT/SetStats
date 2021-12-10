33db28138efbd590ea2fb04bf2680c312f817eb3
Mark Byrne <73395028+MarkByrneDKIT@users.noreply.github.com>
2021-12-10 13:31:48 +0000

    fixed login_status

--- setstatsDB.sql
+++ setstatsDB.sql
@@ -125,7 +125,7 @@ CREATE TABLE `trainee` (
   `trainer_id` int(50) DEFAULT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `login_status` int(50) NOT NULL
+  `login_status` int(50) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -149,7 +149,7 @@ CREATE TABLE `trainer` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `login_status` int(50) NOT NULL
+  `login_status` int(50) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
